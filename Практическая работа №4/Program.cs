1a0282536a31c0aa50fa0c6d9278956a9f7d0be8
SadyLiberty <amirsadriev08@gmail.com>
2026-03-24 15:45:05 +0300

    Update Program.cs

--- Практическая работа №4/Program.cs
+++ Практическая работа №4/Program.cs
@@ -1,70 +1,84 @@
 using System;
 
-internal class Program
+namespace ConsoleApp1
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Cat cat = new Cat();
-        cat.Voice();
-        cat.CountEat();
-        cat.CountHealth();
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
 
-        Dog dog = new Dog();
-        dog.Voice();
+            Console.WriteLine("Добро пожаловать в игру с животными!");
+            Console.WriteLine();
 
-        Parrot parrot = new Parrot();
-        parrot.Voice();
+            cat.Voice();
+
+            Console.Write("Сколько лет кошке? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            cat.ShowAge(age);
+            cat.CountEat();
+            cat.CountHealth();
+
+            Console.WriteLine();
+
+            Dog dog = new Dog();
+            dog.Voice();
+
+            Parrot parrot = new Parrot();
+            parrot.Voice();
+        }
+    }
+
+    interface IAnimal
+    {
+        void Voice();
+    }
+
+    class Cat : IAnimal
+    {
+        public int health = 9;
+        public int dryFood = 2;
+        public int wetFood = 3;
+        public int water = 1;
+
+        public void Voice()
+        {
+            Console.WriteLine("Кошка говорит: мур мур ");
+        }
+
+        public void ShowAge(int age)
+        {
+            Console.WriteLine($"Кошке {age} лет");
+        }
+
+        public void CountHealth()
+        {
+            Console.WriteLine($"Жизней кошки: {health} / еще 9");
+        }
+
+        public void CountEat()
+        {
+            Console.WriteLine($"Сухой корм: {dryFood}");
+            Console.WriteLine($"Влажный корм: {wetFood}");
+            Console.WriteLine($"Воды в день: {water} нигалитров");
+        }
+    }
+
+    class Dog : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Собака говорит: ав ав ");
+        }
+    }
+
+    class Parrot : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Попугай говорит: кар кар");
+        }
     }
 }
-
-interface IAnimal
-{
-    void Voice();
-}
-
-class Cat : IAnimal
-{
-    public int health = 9;
-    public int dryFood = 2;
-    public int wetFood = 3;
-    public int water = 1;
-
-    public void Voice()
-    {
-        Console.WriteLine("Мяу");
-    }
-
-    public void CountHealth()
-    {
-        Console.WriteLine($"Жизней кошки: {health} / 9 еще ");
-    }
-
-    public void CountEat()
-    {
-        Console.WriteLine($"Нормас по сухому корму: {dryFood}");
-        Console.WriteLine($"нормас по влажному корму: {wetFood}");
-        Console.WriteLine($"нормас воды в день: {water} нигалитров");
-    }
-}
-
-class Dog : IAnimal
-{
-    public void Voice()
-    {
-        Console.WriteLine("ав ав ");
-    }
-}
-
-class Parrot : IAnimal
-{
-    public void Voice()
-    {
-        Console.WriteLine("Кар кар ");
-    }
-}
-
-
-
-
-
 
